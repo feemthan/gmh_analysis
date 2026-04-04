f0dcb72d476e937fe391039e6a42f15d7afcb263
faheem <faheem.h@cavininfotech.com>
2026-04-04 11:38:29 +0530

    UPDATE: add sql validator to types

--- src/types.py
+++ src/types.py
@@ -93,3 +93,48 @@ class PipelineOutput:
     # Aggregates
     timings: dict[str, float] = field(default_factory=dict)
     total_llm_stats: dict[str, Any] = field(default_factory=dict)
+
+class SQLValidator:
+    ALLOWED_TABLE = "gaming_mental_health"
+
+    ALLOWED_COLUMNS = {
+        "age",
+        "gender",
+        "income",
+        "daily_gaming_hours",
+        "weekly_sessions",
+        "years_gaming",
+        "sleep_hours",
+        "caffeine_intake",
+        "exercise_hours",
+        "stress_level",
+        "anxiety_score",
+        "depression_score",
+        "social_interaction_score",
+        "relationship_satisfaction",
+        "academic_performance",
+        "work_productivity",
+        "addiction_level",
+        "multiplayer_ratio",
+        "toxic_exposure",
+        "violent_games_ratio",
+        "mobile_gaming_ratio",
+        "night_gaming_ratio",
+        "weekend_gaming_hours",
+        "friends_gaming_count",
+        "online_friends",
+        "streaming_hours",
+        "esports_interest",
+        "headset_usage",
+        "microtransactions_spending",
+        "parental_supervision",
+        "loneliness_score",
+        "aggression_score",
+        "happiness_score",
+        "bmi",
+        "screen_time_total",
+        "eye_strain_score",
+        "back_pain_score",
+        "competitive_rank",
+        "internet_quality",
+    }
